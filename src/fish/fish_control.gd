a0452039a71f0fd1461bf9ef81bcf82d81df225e
Xycydg5150 <geometriccross@gmail.com>
2024-02-12 15:15:58 +0900

    Tweak a fish

--- src/fish/fish_control.gd
+++ src/fish/fish_control.gd
@@ -3,14 +3,6 @@ extends RigidBody2D
 @onready var ray := $point/RayCast2D
 const control = preload("res://src/control_utility.gd")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func _physics_process(delta):
 	self.apply_force(control.input_vector() * 500, Vector2.ZERO)
 	$"ゆ".flip_h = max(-control.input_vector().x, 0)
