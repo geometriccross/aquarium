79631f165253691078e77bd4678e94dbcd2a72a2
Xycydg5150 <geometriccross@gmail.com>
2024-02-12 16:52:14 +0900

    ヒットなどの情報を辞書で返す関数を作成

--- src/sensor.gd
+++ src/sensor.gd
@@ -1,1 +1,20 @@
-extends Node
+extends Area2D
+
+@onready var ray_top := $ray_top
+@onready var ray_mid := $ray_mid
+@onready var ray_low := $ray_low
+
+var entering:Node2D = null
+
+func hit_info():
+	return {
+		"entered_info": entering,
+		"ray_info": {
+			"top": ray_top.get_collider(),
+			"mid": ray_mid.get_collider(),
+			"low": ray_low.get_collider()
+		}
+	}
+
+func _on_body_entered(body):
+	entering = body
