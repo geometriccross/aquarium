38ef349f81e8b3a057e4dc39a0dac8c0cff3d0cb
Xycydg5150 <geometriccross@gmail.com>
2024-02-13 14:58:30 +0900

    画面に魚を一匹追加

--- src/fish.gd
+++ src/fish.gd
@@ -4,9 +4,30 @@ extends RigidBody2D
 @onready var top := $top_sensor
 @onready var low := $low_sensor
 
+@export var manual_control := true
+@export var behavior_interval_max := 3
+@export var behavior_interval_min := 1
+
+@export var move_speed := 400
+
+var _time := 0.0
+
 const control = preload("res://src/control_utility.gd")
 
 func _physics_process(delta):
-	self.apply_force(control.input_vector() * 500, Vector2.ZERO)
-	$"ゆ".flip_h = max(-control.input_vector().x, 0)
-	var viewport_size = get_viewport_rect().size
+	if manual_control:
+		self.apply_force(control.input_vector() * move_speed, Vector2.ZERO)
+		$"ゆ".flip_h = max(-control.input_vector().x, 0)
+	else:
+		_time += delta
+		if _time >= randi_range(behavior_interval_min, behavior_interval_max):
+			behavior_choice([front.hit_info(), top.hit_info(), low.hit_info()])
+			_time = 0
+
+func behavior_choice(info):
+	match info:
+		_:
+			var cranial = randi_range(-1, 1)
+			$"ゆ".flip_h = max(-cranial, 0)
+			self.apply_force(Vector2(cranial, randi_range(-1, 1)) * move_speed, Vector2.ZERO)
+			
