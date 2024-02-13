6706efee93d7b50233ae9828d44d475365fa0282
Xycydg5150 <geometriccross@gmail.com>
2024-02-13 21:18:30 +0900

    行動の関数を変更。行動のパターンを示す関数を追加

--- src/fish.gd
+++ src/fish.gd
@@ -12,6 +12,14 @@ extends RigidBody2D
 
 var _time := 0.0
 
+#左から順にnoraml, escape, focusへと移る確率をあらわした、
+#行動のテーブル
+var behavior_table := {
+	"random": [0.8, 0, 0.2],
+	"escape": [0.3, 0.7, 0],
+	"focus": [0.33, 0.33, 0.33]
+}
+
 const control = preload("res://src/control_utility.gd")
 
 func _physics_process(delta):
@@ -21,13 +29,14 @@ func _physics_process(delta):
 	else:
 		_time += delta
 		if _time >= randi_range(behavior_interval_min, behavior_interval_max):
-			behavior_choice([front.hit_info(), top.hit_info(), low.hit_info()])
 			_time = 0
-
-func behavior_choice(info):
-	match info:
-		_:
-			var cranial = randi_range(-1, 1)
-			$"ゆ".flip_h = max(-cranial, 0)
-			self.apply_force(Vector2(cranial, randi_range(-1, 1)) * move_speed, Vector2.ZERO)
 			
+func random_walk():
+	return Vector2(randi_range(-1, 1), randi_range(-1, 1))
+
+func focus_walk(target: Vector2):
+	return Vector2(transform.origin - target)
+
+func escape_walk(target: Vector2):
+	var v = focus_walk(target)
+	return Vector2(-v.y, -v.x)
