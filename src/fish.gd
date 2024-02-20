5d16f02e0218853e44652e13b408f892fd039ad1
geometriccross <geometriccross@gmail.com>
2024-02-20 20:31:07 +0900

    魚の向きが進行方向により変化するように

--- src/fish.gd
+++ src/fish.gd
@@ -26,16 +26,27 @@ var _behavior_table := {
 const utility = preload("res://src/utility.gd")
 
 func _physics_process(delta):
+	global_rotation = 0 #回転を固定
 	if manual_control:
 		apply_force(utility.input_vector() * move_speed, Vector2.ZERO)
-		$"ゆ".flip_h = max(-utility.input_vector().x, 0)
+		image_flip(utility.input_vector().x)
+
 	else:
 		_time += delta
 		if _time >= randf_range(behavior_interval_min, behavior_interval_max):
 			var f_and_state = behavior_choice(_state, _behavior_table)
-			apply_force(f_and_state[0].call(sensor_perception()) * move_speed * 2, Vector2.ZERO)
+			var vector = f_and_state[0].call(sensor_perception())
+			apply_force(vector * move_speed * 2, Vector2.ZERO)
+			image_flip(vector.x)
 			_state = f_and_state[1]
 			_time = 0
+
+func image_flip(flip_index):
+	#ちょうど0の時には、何もせずそのままの向きを保つ
+	if flip_index > 0:
+		$"ゆ".flip_h = false
+	elif flip_index < 0:
+		$"ゆ".flip_h = true
 
 func sensor_perception() -> Vector2:
 	var vector_num = func(ray): return ((
