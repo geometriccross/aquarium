157bd7b7a159a8864dcca16bd715ac3f650253d2
geometriccross <geometriccross@gmail.com>
2024-02-20 10:23:44 +0900

    近くのオブジェクトに向かって移動するように

--- src/fish.gd
+++ src/fish.gd
@@ -33,30 +33,28 @@ func _physics_process(delta):
 		_time += delta
 		if _time >= randf_range(behavior_interval_min, behavior_interval_max):
 			var f_and_state = behavior_choice(_state, _behavior_table)
-			apply_force(f_and_state[0].call(sensor_perception()) * 2, Vector2.ZERO)
+			apply_force(f_and_state[0].call(sensor_perception()) * move_speed * 2, Vector2.ZERO)
 			_state = f_and_state[1]
 			_time = 0
 
 func sensor_perception() -> Vector2:
 	var vector_num = func(ray): return ((
-			int(ray.hit_info()["ray_info"]["top"] != null)*0.5 + 
+			int(ray.hit_info()["ray_info"]["top"] != null) + 
 			int(ray.hit_info()["ray_info"]["mid"] != null) + 
-			int(ray.hit_info()["ray_info"]["low"] != null)*0.5
-		) / 2
+			int(ray.hit_info()["ray_info"]["low"] != null) 
+		)
 	)
-	
-	var v = Vector2(vector_num.call(front), vector_num.call(low))
-	print(v)
-	return v
-	
+
+	return Vector2(vector_num.call(front), -vector_num.call(top) + vector_num.call(low))
 
 func behavior_choice(state: String, table: Dictionary) -> Array:
+	var p = randf_range(0.0, 1.0)
 	var result = [random_walk, _RANDOM] #もし何も当たらなかった場合に実行される
 	for p_and_f in table[state]:
-		if p_and_f[0] > round(randf_range(0, 1.0)):
+		if p_and_f[0] > p:
 			result = [p_and_f[1], p_and_f[2]]
 
-	print(result)
+	print(result, ", ", p)
 	return result
 
 #この関数の引数は使わない
