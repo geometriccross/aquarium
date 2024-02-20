4107580aeda10976d2c51fe4e56d81f3acb0fb6b
geometriccross <geometriccross@gmail.com>
2024-02-20 22:00:14 +0900

    spawnerの挙動を作成。クリックしたら魚がインスタンスされるように

--- src/spawner.gd
+++ src/spawner.gd
@@ -1,11 +1,10 @@
 extends Node2D
 
+const fish_scene = preload("res://tscn/fish.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		var ins = fish_scene.instantiate()
+		get_tree().get_root().add_child(ins) #これがないとmainシーンに追加されない
+		ins.position = event.position
+		ins.manual_control = false
