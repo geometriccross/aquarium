5a00482a491a565b4b70187a2c96650cb2f63fb6
geometriccross <geometriccross@gmail.com>
2024-02-21 14:02:09 +0900

    行動テーブルをこちらに移動。関数をstaticに変更

--- src/state.gd
+++ src/state.gd
@@ -1,25 +1,32 @@
+var _behavior_table = {
+    "random": 	[[0.4, Idle], [0.5, Random], [0.1, Focus], [0.0, Escape]],
+    "idle": 	[[0.3, Idle], [0.7, Random], [0.0, Focus], [0.0, Escape]],
+    "escape": 	[[0.1, Idle], [0.2, Random], [0.2, Focus], [0.5, Escape]],
+    "focus": 	[[0.2, Idle], [0.2, Random], [0.4, Focus], [0.2, Escape]]
+}
+
 class State: pass
 
 class Idle extends State:
     var mnemonic = "idle"
 
-    func destination_vector(place_holder: Vector2):
+    static func destination_vector(place_holder: Vector2):
         return Vector2(0, 0)
 
 class Random extends State:
     var mnemonic = "random"
 
-    func destination_vector(place_holder: Vector2):
+    static func destination_vector(place_holder: Vector2):
         return Vector2(randi_range(-1, 1), randi_range(-1, 1))
 
 class Escape extends State:
     var mnemonic = "escape"
 
-    func destination_vector(target: Vector2):
+    static func destination_vector(target: Vector2):
         return Vector2(-target.y, -target.x)
 
 class Focus extends State:
     var mnemonic = "focus"
 
-    func destination_vector(target: Vector2):
+    static func destination_vector(target: Vector2):
         return target
