d5994092154323653154edcf7e0c02c8363b714a
geometriccross <geometriccross@gmail.com>
2024-02-21 13:55:30 +0900

    ニモニックコードを設定

--- src/state.gd
+++ src/state.gd
@@ -1,17 +1,25 @@
 class State: pass
 
 class Idle extends State:
+    var mnemonic = "idle"
+
     func destination_vector(place_holder: Vector2):
         return Vector2(0, 0)
 
 class Random extends State:
+    var mnemonic = "random"
+
     func destination_vector(place_holder: Vector2):
         return Vector2(randi_range(-1, 1), randi_range(-1, 1))
 
 class Escape extends State:
+    var mnemonic = "escape"
+
     func destination_vector(target: Vector2):
         return Vector2(-target.y, -target.x)
 
 class Focus extends State:
+    var mnemonic = "focus"
+
     func destination_vector(target: Vector2):
         return target
