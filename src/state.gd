37b106fa5b0c93c3b029281e33569479681ed355
geometriccross <geometriccross@gmail.com>
2024-02-21 14:44:08 +0900

    change a member

--- src/state.gd
+++ src/state.gd
@@ -1,11 +1,12 @@
-var _behavior_table = {
-    "random": 	[[0.4, Idle], [0.5, Random], [0.1, Focus], [0.0, Escape]],
-    "idle": 	[[0.3, Idle], [0.7, Random], [0.0, Focus], [0.0, Escape]],
-    "escape": 	[[0.1, Idle], [0.2, Random], [0.2, Focus], [0.5, Escape]],
-    "focus": 	[[0.2, Idle], [0.2, Random], [0.4, Focus], [0.2, Escape]]
-}
+static func table():
+    return {
+        "random": 	[[0.4, Idle], [0.5, Random], [0.1, Focus], [0.0, Escape]],
+        "idle": 	[[0.3, Idle], [0.7, Random], [0.0, Focus], [0.0, Escape]],
+        "escape": 	[[0.1, Idle], [0.2, Random], [0.2, Focus], [0.5, Escape]],
+        "focus": 	[[0.2, Idle], [0.2, Random], [0.4, Focus], [0.2, Escape]]
+    }
 
-class State: pass
+class State:pass
 
 class Idle extends State:
     var mnemonic = "idle"
