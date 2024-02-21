be3a100a11919432052b6468dbc97e60f5a8e3f6
geometriccross <geometriccross@gmail.com>
2024-02-21 15:11:02 +0900

    add a member

--- src/state.gd
+++ src/state.gd
@@ -1,33 +1,37 @@
 static func table():
-    return {
-        "random": 	[[0.4, Idle], [0.5, Random], [0.1, Focus], [0.0, Escape]],
-        "idle": 	[[0.3, Idle], [0.7, Random], [0.0, Focus], [0.0, Escape]],
-        "escape": 	[[0.1, Idle], [0.2, Random], [0.2, Focus], [0.5, Escape]],
-        "focus": 	[[0.2, Idle], [0.2, Random], [0.4, Focus], [0.2, Escape]]
-    }
+	return {
+		"random": 	[[0.4, Idle], [0.5, Random], [0.1, Focus], [0.0, Escape]],
+		"idle": 	[[0.3, Idle], [0.7, Random], [0.0, Focus], [0.0, Escape]],
+		"escape": 	[[0.1, Idle], [0.2, Random], [0.2, Focus], [0.5, Escape]],
+		"focus": 	[[0.2, Idle], [0.2, Random], [0.4, Focus], [0.2, Escape]]
+	}
 
 class State:pass
 
 class Idle extends State:
-    var mnemonic = "idle"
+	static func mnemonic() -> String:
+		return "idle"
 
-    static func destination_vector(place_holder: Vector2):
-        return Vector2(0, 0)
+	static func destination_vector(place_holder: Vector2):
+		return Vector2(0, 0)
 
 class Random extends State:
-    var mnemonic = "random"
+	static func mnemonic() -> String:
+		return "random"
 
-    static func destination_vector(place_holder: Vector2):
-        return Vector2(randi_range(-1, 1), randi_range(-1, 1))
+	static func destination_vector(place_holder: Vector2):
+		return Vector2(randi_range(-1, 1), randi_range(-1, 1))
 
 class Escape extends State:
-    var mnemonic = "escape"
+	static func mnemonic() -> String:
+		return "escape"
 
-    static func destination_vector(target: Vector2):
-        return Vector2(-target.y, -target.x)
+	static func destination_vector(target: Vector2):
+		return Vector2(-target.y, -target.x)
 
 class Focus extends State:
-    var mnemonic = "focus"
+	static func mnemonic() -> String:
+		return "focus"
 
-    static func destination_vector(target: Vector2):
-        return target
+	static func destination_vector(target: Vector2):
+		return target
